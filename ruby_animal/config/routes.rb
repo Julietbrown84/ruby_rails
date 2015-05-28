c3beb25cf0b6c449be62de0be63f1b56706cb2ef
Juliet Brown <apprentice@StMac4.local>
2015-05-28 19:45:19 +1200

    created comment controller

--- ruby_animal/config/routes.rb
+++ ruby_animal/config/routes.rb
@@ -1,21 +1,25 @@
 Rails.application.routes.draw do
 
-  resources :articles
-  resources :animals, except: :destroy
+    resources :articles do
+    resources :comments
+   end
+    resources :animals, except: :destroy
 
-  # resources :animals, only: [:index, :show]
-  # resources :animals, only: [:index, :show]
-  delete '/articles/:id' => 'articles#destroy'
-  get 'articles' => 'article#index'
-  get '/articles/:id' => 'articles#show'
-  get  '/articles/new' => 'articles#new'
-  get  '/articles/id/edit' => 'articles#new'
-  post '/articles' =>  'articles#create'
 
-  root to: 'animals#index'
-  get '/animals/:id' => 'animals#show'
-  get '/animals/new' => 'animals#new'
-  get '/animals/:id/edit' => 'animals#edit'
+    # resources :animals, only: [:index, :show]
+    # resources :animals, only: [:index, :show]
+    delete '/articles/:id' => 'articles#destroy'
+    get 'articles' => 'article#index'
+    get '/articles/:id' => 'articles#show'
+    get  '/articles/new' => 'articles#new'
+    get  '/articles/id/edit' => 'articles#new'
+    post '/articles' =>  'articles#create'
 
-  get 'welcome/index'
+    root to: 'animals#index'
+    get '/animals/:id' => 'animals#show'
+    get '/animals/new' => 'animals#new'
+    get '/animals/:id/edit' => 'animals#edit'
+
+    get 'welcome/index'
+
 end
