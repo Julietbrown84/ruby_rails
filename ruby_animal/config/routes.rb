075bb047c32d2cf7fd0cfb3e151179b030752779
Juliet Brown <apprentice@StMac4.local>
2015-05-28 18:31:02 +1200

    blog working

--- ruby_animal/config/routes.rb
+++ ruby_animal/config/routes.rb
@@ -5,11 +5,12 @@ Rails.application.routes.draw do
 
   # resources :animals, only: [:index, :show]
   # resources :animals, only: [:index, :show]
-  get
+  get 'articles' => 'article#index'
   get '/articles/:id' => 'articles#show'
   get  '/articles/new' => 'articles#new'
   get  '/articles/id/edit' => 'articles#new'
   post '/articles' =>  'articles#create'
+
   root to: 'animals#index'
   get '/animals/:id' => 'animals#show'
   get '/animals/new' => 'animals#new'
