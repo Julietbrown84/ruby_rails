672effe06380a5d72fd9cc34113c39e4b237c2b7
Juliet Brown <apprentice@StMac4.local>
2015-06-02 16:42:34 +1200

    added first test

--- ruby_animal/spec/controllers/animals_controller_spec.rb
+++ ruby_animal/spec/controllers/animals_controller_spec.rb
@@ -2,10 +2,30 @@ require 'rails_helper'
 
 RSpec.describe Animal, type: :controller do
 
-    describe "fields" do
-      it { should have_db_column(:name).of_type(:string) }
-      it { should have_db_column(:life_story).of_type(:string) }
-      it { should have_db_column(:lives).of_type(:integer).with_options}
+    describe "validations" do
+        it { should validate_presence_of(:name) }
+        it { should validate_presence_of(:life_story) }
+        it { should validate_presence_of(:image_url) }
+    end
+
+    describe "methods" do
+        describe "#lose_a_life!" do
+            context "if cat has more than 1 life remaining" do
+                it "decrements the cat's lives by 1" do
+                    healthy_cat.lose_a_life!
+                    expect(healthy_cat.lives).to eq(8)
+                end
+            end
+            context "if cat has 1 life remaining" do
+                it "removes the cat from the database" do
+                    almost_dead_cat.lose_a_life!
+                    expect(Cat.find_by_name(almost_dead_cat.name)).to be_nil
+                end
+            end
+        end
+    end
+
+end
 
 
 end
