58dc25927ad56a86663c2f158be1806e51bcf68a
Juliet Brown <apprentice@StMac4.local>
2015-06-02 16:53:07 +1200

    added third test

--- ruby_animal/spec/controllers/animals_controller_spec.rb
+++ ruby_animal/spec/controllers/animals_controller_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe AnimalsController, type: :controller do
 
     end
 
-        describe "#show" do
+    describe "#show" do
 
         before do
             @animal = Animal.create(attributes_for(:cat))
@@ -32,4 +32,51 @@ RSpec.describe AnimalsController, type: :controller do
 
     end
 
-end
+    describe "#new" do
+
+        before do
+          get :new
+        end
+
+        it { should respond_with(200) }
+        it { should render_template(:new) }
+
+        end
+
+    end
+
+    describe "#create" do
+
+    context "if valid params" do
+
+      before do
+        @animal_params = attributes_for(:animal)
+        post :create, { animal: @animal_params }
+      end
+
+      it { should respond_with(302) }
+      it "should redirect to the new cat's page" do
+        animal = Animal.find_by(@cat_params)
+        expect(response).to redirect_to("/cats/#{cat.id}")
+      end
+      it "creates a new cat with specified params" do
+        expect(Cat.find_by(@valid_params)).to be_truthy
+      end
+
+    end
+
+    context "if invalid params" do
+
+      before do
+        post :create, { cat: {name: "test"} }
+      end
+
+      it { should respond_with(400) }
+      it { should render_template(:new) }
+      it "should not create a new cat" do
+        expect(Cat.find_by_name("test")).to be_nil
+      end
+
+    end
+
+  end
