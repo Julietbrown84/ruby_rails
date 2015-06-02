e3e019f0bc9cf51c6cf688ab053df64c8e31a014
Juliet Brown <apprentice@StMac4.local>
2015-06-02 17:07:15 +1200

    added first article test

--- ruby_animal/spec/controllers/article_controller_spec.rb
+++ ruby_animal/spec/controllers/article_controller_spec.rb
@@ -2,4 +2,50 @@ require 'rails_helper'
 
 RSpec.describe ArticleController, type: :controller do
 
+
+    describe "#index" do
+
+        before do
+            5.times { Article.create(attributes_for(:article)) }
+            get :index
+        end
+
+        it { should respond_with(200) }
+        it { should render_template(:index) }
+        it "should assign @rticles to all Articles in DB" do
+            expect(assigns(:articles)).to eq(Article.all)
+        end
+
+    end
+
+    describe "#show" do
+
+        before do
+            @article = Article.create(attributes_for(:article))
+            get :show, id: @article.id
+        end
+
+        it { should respond_with(200) }
+        it { should render_template(:show) }
+        it "should assign article with specified id to @article" do
+            expect(assigns(:article)).to eq(@article)
+        end
+
+    end
+
+    describe "#new" do
+
+        before do
+          get :new
+        end
+
+        it { should respond_with(200) }
+        it { should render_template(:new) }
+
+        end
+
+    end
+
+
+
 end
